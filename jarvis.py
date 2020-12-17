eb14219f74781aacf4b5fc5ee82a1adad804f368
kubakuba <jakubkrasniewski@gmail.com>
2020-12-17 15:01:02 +0100

    trying reading calendar data

--- jarvis.py
+++ jarvis.py
@@ -3,7 +3,7 @@ import pyttsx3
 import pandas as pd
 from io import StringIO
 import time
-
+import datetime as dt
 
 def parse_text(text, starting_txt, end_txt, return_rounded_float=False):
     begin = text.find(starting_txt) + len(starting_txt)
@@ -49,14 +49,17 @@ def get_calendar(calendar_url):
     text = requests.get(calendar_url, timeout=10000).text
     calendar_text = parse_text(text, 'content="CalendarSheet', '"><meta name="google"')
     print(calendar_text)
+    engine.say(calendar_text)
     csv_file = StringIO(calendar_text)
     df = pd.read_csv(csv_file, sep=',')
     print(df)
     return df
 
 
-get_calendar('https://docs.google.com/spreadsheets/d/1qkuZfhWN2ZLHsSX0t2_ptC5geoDVSwy4O9dRpeh89NA/edit?usp=sharing')
 engine = pyttsx3.init()
+# engine.say(dt.datetime.now())
+print(dt.datetime.now())
+get_calendar('https://docs.google.com/spreadsheets/d/1qkuZfhWN2ZLHsSX0t2_ptC5geoDVSwy4O9dRpeh89NA/edit?usp=sharing')
 get_symbol_values('Allegro', True)
 
 current_val, opening_val = get_symbol_values('CD Projekt', True)
